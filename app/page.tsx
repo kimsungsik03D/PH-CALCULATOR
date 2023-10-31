58d6cba372098cf6cd5bb1ffc79d7235be27bfa4
sungsik:D <tjdtlr2620@gmail.com>
2023-11-01 02:15:07 +0900

    UPDATE main content layout 생성

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import "./globals.css";
+import { Bill } from "@/components";
 
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="container">
+      <Bill />
+      <Bill />
+      <Bill />
+    </main>
+  );
 }
