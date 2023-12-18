fa4b86ced666fc1ef4beb11b2b56587c16531d32
sungsik:D <tjdtlr2620@gmail.com>
2023-12-18 13:18:21 +0900

    result 카드 섹션 추가

---
+++ images/index.ts
@@ -0,0 +1,19 @@
+import fruit_1 from "./fruit-1.jpeg";
+import fruit_2 from "./fruit-2.jpeg";
+import fruit_3 from "./fruit-3.jpeg";
+import fruit_4 from "./fruit-4.jpeg";
+import fruit_5 from "./fruit-5.jpeg";
+import fruit_6 from "./fruit-6.jpeg";
+import fruit_7 from "./fruit-7.jpeg";
+import fruit_8 from "./fruit-8.jpeg";
+
+export {
+  fruit_1,
+  fruit_2,
+  fruit_3,
+  fruit_4,
+  fruit_5,
+  fruit_6,
+  fruit_7,
+  fruit_8,
+};
