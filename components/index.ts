bec420f2e47e9ba04867eb5c60fd9942ac0aa0b8
sungsik:D <tjdtlr2620@gmail.com>
2024-01-05 01:30:45 +0900

    컴포넌트 Index 수정

--- components/index.ts
+++ components/index.ts
@@ -3,6 +3,7 @@ import Input from "./Input";
 import SelectBox from "./SelectBox";
 import Select from "./Select";
 import Bill from "./Bill";
+import Nav from "./Nav";
 import Widget from "./Widget";
 
-export { Button, Input, SelectBox, Select, Bill, Widget };
+export { Button, Input, SelectBox, Select, Bill, Nav, Widget };
